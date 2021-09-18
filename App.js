f7a5f72503f6912e52829f1681080aa3e204ca99
Debora Ribeiro de Souza <deboraribeiror123@gmail.com>
2021-09-17 22:05:36 -0300

    full application 🚀

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Bitcoin from './src/Bitcoin';
+
+export default function App() {
+    return(
+      <Bitcoin/>
+
+    )
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+  },
+});
